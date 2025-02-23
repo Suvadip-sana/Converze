a7f99a6630208048ddab0e94a5c9c47dc0be1e00
Suvadip-sana <suvadipsana602@gmail.com>
2025-02-24 01:21:53 +0530

    Update main server

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,28 +1,30 @@
+import dotenv from "dotenv"; // ✅ ES Modules syntax
+if (process.env.NODE_ENV !== "production") {
+    dotenv.config(); // Load .env file
+}
 import express from "express";
 import mongoose from 'mongoose';
 import {createServer} from "node:http";
 import {Server} from "socket.io";
 import cors from 'cors';
-
 import { connectToSocket } from "./controllers/socket.manager.js"
-// import { connectToSocket } from "./backups/socket.manager.js"
-
 import userRoutes from "./routes/user.routes.js";
 
 
 const port = process.env.PORT || 7000;
-const dburl = process.env.DB_URL || 'mongodb+srv://suvadipsana19:V3u3Uc3ppjXdJ1kb@cluster0.sf4zg.mongodb.net/';
+const dburl = process.env.DB_URL
 
 const app = express();
 const server = createServer(app); // The ocket server and the express instance is different, So here might be something to conect both, So here comes 'createServer'
 const io = connectToSocket(server);
 
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://converze.onrender.com',
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
-
 
 async function connectMong(){
     await mongoose.connect(dburl);
@@ -34,12 +36,8 @@ connectMong().then(()=> {
     console.log("Faild to connect with DB, error: ", err);
 });
 
-// app.get("/", (req, res) => {
-//     res.send("Work fine!")
-// });
-
-app.use("/api/v1/users", userRoutes); // Append the user route in main server.
-
+// app.use("/api/v1/users", userRoutes); // Append the user route in main server.
+app.use("/api/v2/users", userRoutes); // Append the user route in main server.
 
 server.listen(port, ()=> {
     console.log("Server listening on port: ", port);
