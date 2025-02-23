e4f4eaa884ad6c039e937f53ae67b8e537311721
Suvadip-sana <suvadipsana602@gmail.com>
2025-02-23 22:32:54 +0530

    Error fixed

--- backend/src/app.js
+++ backend/src/app.js
@@ -38,7 +38,7 @@ app.get("/", (req, res) => {
     res.send("Work fine!")
 });
 
-app.use("/api/v1/users", userRoutes); // Append the user route in main server.
+app.use("api/v1/users", userRoutes); // Append the user route in main server.
 
 
 server.listen(port, ()=> {
