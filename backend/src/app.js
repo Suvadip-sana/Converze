b6ccfef11683a76b3020286daba4abc4ca83f052
Suvadip-sana <suvadipsana602@gmail.com>
2025-04-25 00:25:25 +0530

    Not found error fixed

--- backend/src/app.js
+++ backend/src/app.js
@@ -30,11 +30,11 @@ app.use(express.urlencoded({extended: true}));
 
 
 // Serve static files from the React/Vue build folder
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 // Catch-all route to serve index.html for client-side routes
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 async function connectMong(){
